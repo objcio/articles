adc286f775ece932dccb0c2496cdf93e99d69717
Nick Lockwood <nick@charcoaldesign.co.uk>
2014-05-02 11:05:41 +0100

    Added code projects and deleted abandoned article

---
+++ issue-12-custom-layer-property-animations/AudioLayer/AudioLayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AudioLayer
+//
+//  Created by Nick Lockwood on 29/04/2014.
+//  Copyright (c) 2014 Charcoal Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
